bfe230bc776793f33ba66b99481461299266aab0
JBL-Oussema <jbeli.oussema00@gmail.com>
2026-04-19 19:43:04 +0100

    optimization recherche images

--- app/liste-des-stades/page.tsx
+++ app/liste-des-stades/page.tsx
@@ -31,8 +31,9 @@ export default async function PitchesList() {
                 alt={pitch.nom || 'Pitch Image'}
                 fill
                 className="object-cover transition-transform duration-500 group-hover:scale-110"
-                loading="lazy"
+                loading="eager"
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                unoptimized
               />
             </div>
             
